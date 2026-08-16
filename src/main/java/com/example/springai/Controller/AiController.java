6bd3eb52c17cc8e0fad34ec95dfe8811d28f2d44
omcar <omcartest@gmail.com>
2026-08-16 14:50:10 +0530

    Prompt class detail , inside call function which was used previously

--- src/main/java/com/example/springai/Controller/AiController.java
+++ src/main/java/com/example/springai/Controller/AiController.java
@@ -1,10 +1,13 @@
 package com.example.springai.Controller;
 
 import com.example.springai.Service.ChatModelService;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 @RestController
 public class AiController {
@@ -17,6 +20,11 @@ public class AiController {
 
     @GetMapping("/chat")
     public void chatWithModel(@RequestParam String message){
-        System.out.println(chatModelService.callChatModel(message));
+        ChatResponse chatResponse= chatModelService.callChatModel(message);
+//        chatResponse.getResult().getOutput().getText() -> for messsage ( message converted to prompt nd send to model)
+//        System.out.println(chatResponse.getResult().getOutput().getText());
+//        System.out.println(chatResponse);
+        System.out.println(chatResponse.getMetadata().getModel());
+        System.out.println(chatResponse.getResult().getOutput().getText());
     }
 }
--- src/main/java/com/example/springai/Service/ChatModelService.java
+++ src/main/java/com/example/springai/Service/ChatModelService.java
@@ -1,6 +1,8 @@
 package com.example.springai.Service;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,9 @@ public class ChatModelService {
         this.chatModel=model;
     }
 
-    public String  callChatModel(String message){
-        return chatModel.call(message);
+    public ChatResponse callChatModel(String message){
+        Prompt prompt= new Prompt(message);
+        System.out.println(prompt.getUserMessage().getText());
+        return chatModel.call( prompt);
     }
 }
